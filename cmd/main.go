82953e619ac5225209a0cd2a7860d764a871506c
Eric Masi <eric.masi.dev@gmail.com>
2025-08-06 09:28:55 -0400

    Refactor

--- cmd/main.go
+++ cmd/main.go
@@ -133,22 +133,12 @@ func main() {
 		http.SetCookie(w, cookie)
 
 		if count == len(ingredients) {
-			cookie, err := cs.GetStepCookie()
+			cookie, err := cs.ToNextStep()
 			if err != nil {
 				logger.Error(err.Error())
 				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 				return
 			}
-
-			step, err := recipes.ParseRecipeStep(cookie.Value)
-			if err != nil {
-				logger.Error(err.Error())
-				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-				return
-			}
-
-			cookie.Path = "/"
-			cookie.Value = step.GetNextStep().String()
 
 			http.SetCookie(w, cookie)
 
